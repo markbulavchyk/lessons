f90acb8a4b6dc46f1f8efcedf4a9354460950983
Mark <marksaitiev@gmail.com>
2023-07-26 22:01:18 -0600

    lesson32 objects and destructuring objects

--- js/script.js
+++ js/script.js
@@ -1,25 +1,40 @@
 "use strict";
-function first () {
-    setTimeout(function(){
-        console.log(1);
-    },500); // 500 милисекунд
-}
-function second () {
-    console.log(2);
-}
-first();
-second();
 
-function learnJS(lang , callback) {
-    console.log(`я учу: ${lang}`);
-    callback();
-}
-learnJS('JavaScript', function() {
-    console.log('я прошел етот урок!');
-})  
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function() {
+        console.log('test')
+    }
+};
 
+// console.log(Object.keys(options))
+
+options.makeTest();
 
 
-console.log(isNaN('s'));
+const {border, bg} = options.colors;
+console.log(border);
 
+
+// delete options.colors;
+
+
+let count = 0;
+for (let key in options) {
+    // if (typeof(options[key]) === 'object') {
+    //     for (let i in options[key]) {
+    //         console.log(`Свойство ${i} имеет значение ${options[key][i]}`)
+    //     }
+    // } else {
+    //     console.log(`Свойство ${key} имеет значение ${options[key]}`)
+    // }
+    count++
+};
+console.log(count)
 

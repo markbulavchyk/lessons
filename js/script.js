99da6cb59861a0977cf2cbc2bcef0d87a50d9b94
Mark <marksaitiev@gmail.com>
2023-07-26 22:48:31 -0600

    lesson37,practice4, addObjects

--- js/script.js
+++ js/script.js
@@ -1,33 +1,77 @@
-"use strict";
+/* Задание на урок:
+1) Первую часть задания повторить по уроку
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
+P.S. Функции вызывать не обязательно */
 
-let str = 'some';
-let strObj = new String(str);
+'use strict';
 
-// console.log(typeof(str));
-// console.log(typeof(strObj));
-
-// console.dir([1,2,3]);
-
-const soldier = {
-    health: 400,
-    armor: 100,
-    sayHello: function () {
-        console.log('hello');
+const personalMovieDB = {
+    count: 0,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+    start: function() {
+        personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+        }
     },
-    callbackFunc: function (name) {
-        console.log(`Имя солдата:${name}`);
+    rememberMyFilms: function() {
+        for (let i = 0; i < 2; i++) {
+            const a = prompt('Один из последних просмотренных фильмов?', ''),
+                  b = prompt('На сколько оцените его?', '');
+        
+            if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+                personalMovieDB.movies[a] = b;
+                console.log('done');
+            } else {
+                console.log('error');
+                i--;
+            }
+        }
+    },
+    detectPersonalLevel: function() {
+        if (personalMovieDB.count < 10) {
+            console.log("Просмотрено довольно мало фильмов");
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+            console.log("Вы классический зритель");
+        } else if (personalMovieDB.count >= 30) {
+            console.log("Вы киноман");
+        } else {
+            console.log("Произошла ошибка");
+        }
+    },
+    showMyDB: function (hidden) {
+        if (!hidden) {
+            console.log(personalMovieDB);
+        }
+    },
+    toggleVisibleMyDB: function () {
+        if (personalMovieDB.privat) {
+            personalMovieDB.privat = false;
+        } else {
+            personalMovieDB.privat = true;
+        }
+    },
+    writeYourGenres: function() {
+        for (let i = 1; i <= 3; i++) {
+            let genre = prompt(`Ваш любимый жанр под номером ${i}`);
+
+            if (genre == '' || genre == null ) {
+                console.log('Вы ввели неккоретные данные или не ввели их вовсе');
+                i--
+            } else {
+            personalMovieDB.genres[i - 1] = genre;
+            }
+        }
+        personalMovieDB.genres.forEach((item, i) => {
+            console.log(`Любимый жанр ${i + 1} - это ${item}`);
+        })
     }
-}
-
-const john = Object.create(soldier);
-
-// const john = {
-//     health: 100
-// };
-
-// john.__proto__ = soldier;
-
-// Object.setPrototypeOf(john, soldier);
-// console.log(john.armor);
-
-john.callbackFunc(` John`);
+};

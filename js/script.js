1c4d67472c76b758e539d78927a7963c79e95f06
Mark <marksaitiev@gmail.com>
2023-07-26 21:53:39 -0600

    lesson28,number methods and properties

--- js/script.js
+++ js/script.js
@@ -1,124 +1,24 @@
 "use strict";
 
-const usdCurr = 28;
-const discount = 0.9;
+const test = '12.2px' 
 
-function convert (amount, curr) {
-    return curr * amount;
-}
+console.log(parseFloat(test));
 
-function promotion (result) {
-     console.log(result * discount);
-}
-promotion (convert(500, usdCurr));
+const str = 'test';
+const arr = [1,2,4];
+// console.log(arr.length);
 
 
 
-function test () {
-    for(i = 0; i < 5; i++) {
-        console.log(i);
-        if (i === 3) return
-    }
-    console.log('Done');
-}
-test();
+const QWER = str.toUpperCase();
+console.log(QWER);
 
-function doNothing() {};
-console.log(doNothing() === undefined);
+const fruit = 'some fruit';
 
+console.log(fruit.indexOf("fruit"))
 
-// Пример: вызов функции sayHello('Антон') 
-// возвращает строку "Привет, Антон!". 
-//В решении вызывать функцию не нужно, программа сделает это за вас.
+console.log(fruit.slice(0,4));
 
-// P.S. возвращать - это использовать ключевое слово return.
+const testik = '12.4';
 
-function sayHello(name) {
-    return  `Привет, ${name}!`
-}
-sayHello();
-
-
-/*2) Создайте функцию,
-
- которая принимает в себя 1 аргумент в виде целого числа
-
- и возвращает массив из трех чисел: одно на 1 меньше, сам аргумент, и число на 1 больше.
-
-Пример: вызов функции returnNeighboringNumbers(5) возвращает массив в виде [4, 5, 6]. */
-
-function number (num) {
-    num = 1;
-    const arr = [0,1,2];
-    return arr
-}
-number();
-console.log(number());
-
-
-// Создайте функцию, которая будет принимать в себя 2 аргумента, оба числа. 
-// Первое число - это база, второе число - это сколько раз нужно будет повторить это число в прогрессии.
-//  Обратите внимание на прогрессию, она рассчитывается по простой формуле умножения. 
-//  Если первый аргумент 5, а второй 3, то число повторяется 3 раза, каждый раз увеличиваясь на само себя. 
-//  Это базовая математика, которая и нужна для работы в 95% случае на фронтенде.
-
-/*
-(Смотри пример ниже). 
-Функция должна возвращать строку 
-(или число в особых случаях, о которых ниже), 
-где эти числа идут по порядку, разделенные тремя дефисами "---". 
-После последнего числа их не должно быть.
-
-Если второй аргумент не является числом, равен или меньше нуля - то возвращать просто первый аргумент. 
-(Проверяем через оператор typeof)
-
-Примеры:
-
-Вызов функции getMathResult(5, 3) даст ответ 5---10---15
-
-Вызов функции getMathResult(3, 10) даст ответ 3---6---9---12---15---18---21---24---27---30
-
-Вызов функции getMathResult(10, 5) даст ответ 10---20---30---40---50
-
-Вызов функции getMathResult(10, '5') даст ответ 10
-
-Вызов функции getMathResult(10, 0) даст ответ 10
-
-Вызов функции getMathResult(20, -5) даст ответ 20
-
-Эта задача уже ближе к реальности, когда вам нужно учитывать и тип данных у аргументов, проверять их и продумывать логику работы внутри.
- Обратите внимание на прогрессию, она рассчитывается по простой формуле умножения. 
- Если первый аргумент 5, а второй 3, то число повторяется 3 раза, каждый раз увеличиваясь на само себя. 
- Это базовая математика, которая и нужна для работы в 95% случае на фронтенде. */
-
- function qwerty (celoe, kolvoPovtorov) {
-    let emptyString = '';
-
-    for (let i = 1; i <= kolvoPovtorov; i++) {
-        emptyString += `${celoe * i}---`
-    }
-    return emptyString
-}
-console.log(qwerty(10, 5))
-
-
-function getMathResult(num, times) {
-    if (typeof(times) !== 'number' || times <= 0) {
-        return num;
-    }
-    let str = '';
-
-    for (let i = 1; i <= times; i++) {
-        if (i === times) {
-            str += `${num * i}`;
-            // Тут без черточек в конце
-        } else {
-            str += `${num * i}---`;
-            // Это тоже самое, что и
-            // str = str + num * i + "---"
-        }
-    }
-    return str;
-}
-
-console.log(getMathResult(10, 3));
+console.log(parseFloat(testik));

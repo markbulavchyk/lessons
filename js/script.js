9ab1936c0e3313df03b82be1f4751b10b9d3fb28
Mark <marksaitiev@gmail.com>
2023-07-26 21:31:09 -0600

    lesson22,cycles_while,do,for

--- js/script.js
+++ js/script.js
@@ -1,53 +1,29 @@
 "use strict";
 
-// Условия if - если , else - если 2 , если неверное условие if 
+let num = 50;
 
-// const hamburger = 5;
-// const fries = undefined;
 
-// if (hamburger && fries) {
-//     console.log('i am full');
+// while (num <= 55) { // цикл Пока переменная нам будет меньше 55 будут выполнятся определенные действия
+//     console.log(num);
+//     num++;
 // };
 
-// console.log(1 && 0); 
-// console.log(1 && 5); 
-// console.log(null && 5); 
-// console.log(0 && 'jnrqqjirq'); 
 
-const hamburger = 3;
-const fries = 3;
-const cola = 0;
-const nuggets = 2;
-
-// i + , ili menshe
-if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
-    console.log('Not hungry');
-} else {
-    console.log('We go');
-};
-console.log(hamburger === 3 && cola === 2 || fries === 3 && nuggets);
-
-console.log(!0);
-
-let johnReport,
-    alexReport,
-    samReport,
-    mariaReport = 'done';
-
-console.log(johnReport || alexReport || samReport || mariaReport);
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num <= 55);
 
 
+// КОМБИНИРУЕМ С УСЛОВИЕМ . если наша переменная i будет равна 6 , расскрываем условия и ставим брейк
+// break - оператор который полностью прирывает цикл   ,дальше не продолжается 
+for (let i = 1; i < 10; i++) { 
+    if(i === 6) {
+        // break; 
+        continue; // оператор который  позволяет пропустить тот шаг который нам не нужен , 
+                  // но при этом не прирывает полностью цикл 
 
-let hamburgerA;
-const friesA = 3;
-const colaA = 0;
-const nuggetsA = 2;
-
-
-if (hamburgerA && colaA || friesA === 3 && nuggetsA) {
-   console.log('Done!')
-   console.log((hamburgerA && colaA || friesA === 3 && nuggetsA))
+    }
+    console.log(i);
 }
-
-
-

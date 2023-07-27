d30d9c48fb7eeb232e0b5138913d82b14dcca92d
Mark <marksaitiev@gmail.com>
2023-07-26 21:45:06 -0600

    lesson24,practice2

--- js/script.js
+++ js/script.js
@@ -1,230 +1,85 @@
-for (i = 20; i > 10; i--) {
-    console.log(i);
-    if (i === 14) {
-        break;
-    }
-};
+"use strict";
+/* Задание на урок:
 
-let i = 4;
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+// const a = prompt('Один из последних просмотренних фильмов?',''), // '' - в кавычках будет  ответ пользователя на вопрос:
+//       b = prompt('На сколько оцените его?',''),
+//       c = prompt('Один из последних просмотренних фильмов?',''),
+//       d = prompt('На сколько оцените его?','');
+
+// personalMovieDB.movies[a] = b; 
+ // movies[prompt('Один из последних просмотренних фильмов?','')] - ответ 1 в консоль: 
+ //                                                  '' - в кавычках будет  ответ пользователя на вопрос:
+ //personalMovieDB.movie[ответ пользователя  например Халк:  присваивает значение = [b] - ответ пользователя на второй вопрос ОЦЕНКА 
+// personalMovieDB.movies[c] = d;
+
+
+// movies: {
+ //   "название_фильма_1": "оценка_1",
+ //   "название_фильма_2": "оценка_2"
+// }
+
+
+
+// for (let i = 0; i < 2; i++) {
+//     const a = prompt('Один из последних просмотренних фильмов?',''), // '' - в кавычках будет  ответ пользователя на вопрос:
+//           b = prompt('На сколько оцените его?','');
+
+//     if (a != '' && b != '' && a != null && b != null && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         i--;
+//     }
+// }
+let i = 0;
 do {
-    i++;
-    console.log(i);
-}
-while (i<10);
-    
-    
-for (j=2; j<=10;j++) {
-     if(j % 2 !== 0) {
-        continue;
-     } 
-     console.log(j);
-};
-
-
-
-const obj = {
-    name: ' john',
-    age: 25
-};
-console.log(obj);
-
-
-// MACUB
-
-const arrayOfNumbers = [];
-
-for (let i = 5; i < 11; i++) {
-        arrayOfNumbers[i - 5] = i;
-    }
-
-console.log(arrayOfNumbers);
-return arrayOfNumbers;
-
-
-let arr = [5,6,7,8,9,10];
-console.log(arr);
-
-
-
-
-
-
-let i = 2;
-while (i <= 15) {
-    i++;
-    if (i % 2 === 0) {
-    continue;
-    } else {
-    console.log(i);
-    }
-}
-
-
-
-
-"use strict";
-first: for (let i = 0; i < 3; i++) {
-    console.log(`first lvl ${i}`);
-    
-    for(let j = 0; j < 3; j++) {
-        console.log(`second lvl ${j}`);
-        
-        for(let k = 0; k < 3; k++) {
-            if (k === 2) continue first;
-            console.log(`third lvl ${k}`);
+    const a = prompt('Один из последних просмотренних фильмов?',''), // '' - в кавычках будет  ответ пользователя на вопрос:
+          b = prompt('На сколько оцените его?','');
+          if (a != '' && b != '' && a != null && b != null && a.length < 50) {
+            i++;
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
         }
-    }
-};
-
-
-console.log();
-
-/*Заполните новый массив (result) числами из старого (arr). 
-
-Количество элементов в массиве можно получить как arr.length,
-
- а к элементам обращаемся все так же: arr[0], arr[1] и тд.
-
-Должен получиться точно такой же массив
-*/
-
-// Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
-const arr = [3, 5, 8, 16, 20, 23, 50];
-
-const result = [];
-
-for (i = 0; i < arr.length; i++) {
-    result[i] = arr[i];
-}
-console.log(result);
+} while (i < 2);
 
 
 
-
-/* Измените данный массив так, чтобы все числа были увеличены в 2 раза, 
-const data = [5, 10, 'Shopping', 20, 'Homework'];
-
- Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
-
-а если попадается строка строка - то к ней было добавлено " - done".
-
-Для определения типа данных используйте typeof();
-
-Должно получиться: [ 10, 20, 'Shopping - done', 40, 'Homework - done' ]
-
-*/
-
-
-"use strict";
-const data = [5, 10, 'Shopping', 20, 'Homework'];
-
-for (let i = 0; i < data.length; i++) {
-    if (typeof data[i] === 'number') {
-        data[i] = data[i] * 2 ;
-    } else if (typeof data[i] === 'string') {
-        data[i] = data[i] + ' - done';  
-    }
+//  При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+// "Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+// "Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов")
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    console.log("Вы классический зритель")
+} else if (personalMovieDB.count > 30) {
+    console.log("Вы киноман")
+} else {
+    console.log("Произошла ошибка")
 }
 
-console.log(data);
-
-
-/*  Разверните массив data наоборот при помощи цикла и запишите данные в массив result.
-
-    const data = [5, 10, 'Shopping', 20, 'Homework'];
-
-    Должно получиться: [ 'Homework', 20, 'Shopping', 10, 5 ]
-
-*/
-
-    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
-
-const data = [5, 10, 'Shopping', 20, 'Homework'];
-const result = [];
-
-for (i = 0; i < data.length; i++) {
-    result[0] = data[4];
-    result[1] = data[3];
-    result[2] = data[2];
-    result[3] = data[1];
-    result[4] = data[0];
-};
-console.log(result);
-
-
-
-"use strict";
-const data = [5, 10, 'Shopping', 20, 'Homework'];
-const result = [];
-                                //5
-    for (let i = 1; i <= data.length; i++) {
-    
-                                //    iteration 1
-        // result[0] = data [5-1] 
-        result[i - 1] = data[data.length - i]
-    }
-console.log(result);
-
- 
-"use strict";
-const data = [5, 10, 'Shopping', 20, 'Homework'];
-console.log(data.length);
-const result = [];
-    for (let i = 1; i <= data.length; i++) {
-        result[i - 1] = data[data.length - i];
-    }
-console.log(result);
-
-let i = 1;
-for (; i < 5; i++) {
-    
-}
-
-console.log(i)
-
-
-
-<<<<<<< HEAD
-// ЦИКЛ В ЦИКЛЕ
-=======
-// 
->>>>>>> refs/remotes/origin/main
-
-for (let i = 0; i < 3; i++) {
-    console.log(i);
-    for(let j = 0; j < 3; j++) {
-        console.log(j);
-    }
-};
-
-let result = '';
-for (let i = 1; i < 7; i++) {
-    for (let j = 0; j < i; j++) {
-        result += '*';
-        
-    }
-    result += '\n';
-}
-<<<<<<< HEAD
-console.log(result); 
-=======
-console.log(result);
->>>>>>> refs/remotes/origin/main
-
-
-
-// 0 - повторение первого цикла (Родительского) 1 раз
-// 0 - повторение дочерного цикла  (Цикл в цикле < j > ) 1 раз
-// 1 - повторение дочерного цикла  (Цикл в цикле < j > ) 2 раз 
-// 2 - повторение дочерного цикла  (Цикл в цикле < j > ) 3 раз 
-// 1 - повторение первого цикла (Родительского) 2 раз
-// 0 - повторение дочерного цикла  (Цикл в цикле < j > ) 4 раз
-// 1 - повторение дочерного цикла  (Цикл в цикле < j > ) 5 раз
-// 2 - повторение дочерного цикла  (Цикл в цикле < j > ) 6 раз
-// 2 - повторение первого цикла (Родительского) 3 раз 
-// 0 - повторение дочерного цикла  (Цикл в цикле < j > ) 7 раз
-// 1 - повторение дочерного цикла  (Цикл в цикле < j > ) 8 раз
-// 2 - повторение дочерного цикла  (Цикл в цикле < j > ) 9 раз
-
-
-
+console.log(personalMovieDB);

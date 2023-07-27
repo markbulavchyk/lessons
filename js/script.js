586c78d3b260a60600e7be955da1d20acf9aa37d
Mark <marksaitiev@gmail.com>
2023-07-26 21:58:35 -0600

    lesson30 ,add method trim()

--- js/script.js
+++ js/script.js
@@ -1,35 +1,79 @@
+/* Задание на урок:
+1) Первую часть задания повторить по уроку
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
+P.S. Функции вызывать не обязательно */
 
-function calculateVolumeAndAre(num) {
-    let str = `Объем куба: ${num * num * num},Площадь всей поверхности: ${(num * num)* 6}`;
-    if (typeof(num) !== 'number' || num <= 0 || !Number.isInteger(num))  {
-        return "При вычислении произошла ошибка";
-    } else {
-        return str
+'use strict';
+
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
     }
 }
-console.log(cub(13))
+
+start();
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
 
-// Функция принимает только целое число от 1 до 36.
-
-// Если переданный аргумент не число, отрицательное или дробное - возвращается сообщение:
-
-// "Ошибка. Проверьте правильность введенного номера места"
-
-// Если число 0 или больше 36, то сообщение: "Таких мест в вагоне не существует"
-
-function searchPlace(argument) {
-    if (typeof(argument) !== 'number' || argument < 0 || argument !== parseInt(argument) ) {
-        console.log('Ошибка. Проверьте правильность введенного номера места');
-    } else if (argument === 0 || argument > 36) {
-        console.log('Таких мест в вагоне не существует');
-    }
-
-    else {
-        console.log(Math.ceil(argument / 4));
-    }
-
-    // return Math.ceil(argument / 4);
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', '').trim(),
+              b = prompt('На сколько оцените его?', '');
     
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
+    }
 }
-searchPlace(3);
+
+rememberMyFilms();
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log("Просмотрено довольно мало фильмов");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log("Вы классический зритель");
+    } else if (personalMovieDB.count >= 30) {
+        console.log("Вы киноман");
+    } else {
+        console.log("Произошла ошибка");
+    }
+}
+
+detectPersonalLevel();
+
+function showMyDB (hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+    }
+}
+
+
+writeYourGenres(); 

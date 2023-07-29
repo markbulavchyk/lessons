313f81cbe4778369896b596ea63ebc3605f56bfb
Mark <marksaitiev@gmail.com>
2023-07-29 16:27:57 -0600

    lesson39 dynamic_types

--- js/script.js
+++ js/script.js
@@ -1,21 +1,58 @@
 "use strict";
 
-function hello () {
-    console.log('Hello World');
-};
+// to String
 
-hello();
+// 1) превращаем в строку 
 
-function hi () {
-    console.log('Say hi');
-};
+console.log(typeof(String(null)));
+console.log(typeof(String(4)));
 
-hi();
+// 2)
+
+console.log(typeof(5 + ''));
+
+const num = 5;
+
+console.log('https://vk.com/catalog/' + num);
 
 
-const arr = [1,14,4,30,54],
-      sorted = arr.sort(compareNum);
-      
-function compareNum(a,b) {
-    return a - b;
+const fontSize = 26 + 'px';
+
+console.log();
+
+// To Number
+
+//1) 
+
+console.log(typeof(Number('4')));
+
+//2) унарный плюс
+
+console.log(typeof+('4'));
+
+// 3)
+
+console.log(typeof(parseInt('15px',10)));
+
+
+
+let answ = +prompt('hello', '');
+
+
+// To boolean
+
+// 1 -й способ
+
+let switcher = null;
+
+if(switcher) {
+    console.log('Working');
 }
+
+// 2-й способ
+
+console.log(typeof(Boolean('4')));
+
+// 3-й способ 
+
+console.log(typeof(!!'4'));

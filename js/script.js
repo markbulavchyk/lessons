bbd0c1721cd2faa1db77f17dda6d3de72e1078c4
Mark <marksaitiev@gmail.com>
2023-08-20 20:06:17 -0600

    lesson49, events on mobile devices

--- js/script.js
+++ js/script.js
@@ -1,92 +1,39 @@
 'use strict';
 
-document.addEventListener('DOMContentLoaded', () => {
+// touchstart 
 
-    const movieDB = {
-        movies: [
-            "Логан",
-            "Лига справедливости",
-            "Ла-ла лэнд",
-            "Одержимость",
-            "Скотт Пилигрим против..."
-        ]
-    };
+// touchmove
 
-    const adv = document.querySelectorAll('.promo__adv img'),
-        poster = document.querySelector('.promo__bg'),
-        genre = poster.querySelector('.promo__genre'),
-        movieList = document.querySelector('.promo__interactive-list'),
-        addForm = document.querySelector('form.add'),
-        addInput = addForm.querySelector('.adding__input'),
-        checkbox = addForm.querySelector('[type="checkbox"]');
+// touchend
 
-     addForm.addEventListener('submit', (event) => {
-        event.preventDefault();
+// touchenter
 
-        let newFilm = addInput.value;
-        const favorite = checkbox.checked;
+// touchleave 
 
-        if(newFilm) {
-            if (newFilm.length > 21) {
-                newFilm = `${newFilm.substring(0,22)}...`;
-            }
-            if (favorite) {
-                console.log('Добавляем любимый фильм');
-            }
+// touchcancel
 
-            movieDB.movies.push(newFilm);
-            sortArr(movieDB.movies);
-    
-            createMovieList(movieDB.movies, movieList);
-        }
+window.addEventListener('DOMContentLoaded' , () => {
+    const box = document.querySelector('.box');
+    box.style.cssText = `width: 150px;
+                     height: 100px;
+                     background-color: red`;
 
-       
+    box.addEventListener('touchstart', (e) => {
+        e.preventDefault();
 
-        event.target.reset();
-     });
+        console.log('start');
+        console.log(e.targetTouches);
+    })
 
-    const deleteAdv = (arr) => {
-        arr.forEach (item => {
-            item.remove();
-        });
-    }
+    box.addEventListener('touchmove', (e) => {
+        e.preventDefault();
 
-    
-
-    const makeChanges = () => {
-        genre.textContent = 'драма';
-
-        poster.style.backgroundImage = 'url("img/bg.jpg")'
-    };
-
-
-    const sortArr = (arr) => {
-        arr.sort();
-    }
-
-
-
-    function createMovieList (films, parent) {
-        parent.innerHTML = '';
-        sortArr(films);
-        films.forEach((film,i) => {
-            parent.innerHTML += `
-                <li class="promo__interactive-item">${i+ 1} ${movieDB.movies[i]}
-                    <div class="delete"></div>
-                </li>
-            `;
-        });
-
-        document.querySelectorAll('.delete').forEach((btn,i) => {
-            btn.addEventListener('click', () => {
-                btn.parentElement.remove();
-                movieDB.movies.splice(i,1);
-                createMovieList(films, parent);
-            })
-        });
-    }
-    deleteAdv(adv);
-    makeChanges();
-    createMovieList(movieDB.movies, movieList);
+        console.log(e.targetTouches[0].pageX);
+    })
 
 });
+
+// touches 
+// targetTouches
+//changesTouches - пальцы которые совершили указанное действие 
+
